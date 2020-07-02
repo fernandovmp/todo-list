03c6f170d936fe9923e8e4b545169519c6196a94
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-02 18:32:15 -0300

    Register db connection and todo item repository in the services collection

--- src/TodoList.WebApi/Startup.cs
+++ src/TodoList.WebApi/Startup.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TodoList.WebApi.Repositories;
 
 namespace TodoList.WebApi
 {
@@ -25,6 +28,11 @@ namespace TodoList.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddTransient<IDbConnection>(sp =>
+                new SqlConnection(Configuration.GetConnectionString("SqlServerConnection")));
+
+            services.AddScoped<ITodoItemRepository, TodoItemRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
