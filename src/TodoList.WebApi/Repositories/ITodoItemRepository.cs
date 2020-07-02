dae54af7641a71e013ae54138dd3520628052a5a
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-02 18:31:16 -0300

    Add todo item repository in web api project

---
+++ src/TodoList.WebApi/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TodoList.WebApi.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        Task<IEnumerable<TodoItem>> GetTodoItems();
+        Task<TodoItem> GetTodoById(int id);
+        Task Insert(TodoItem todoItem);
+        Task Update(int id, TodoItem todoItem);
+        Task Delete(int id);
+    }
+}
---
+++ src/TodoList.WebApi/Repositories/TodoItemRepository.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace TodoList.WebApi.Repositories
+{
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public TodoItemRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public async Task<IEnumerable<TodoItem>> GetTodoItems()
+        {
+            string query = @"select id, title, completed from TodoItems;";
+            IEnumerable<TodoItem> todoItems = await _connection.QueryAsync<TodoItem>(query);
+            return todoItems;
+        }
+        public async Task<TodoItem> GetTodoById(int id)
+        {
+            string query = @"select id, title, completed from TodoItems where id = @Id;";
+            TodoItem todoItem = await _connection.QuerySingleOrDefaultAsync<TodoItem>(query, new
+            {
+                Id = id
+            });
+            return todoItem;
+        }
+        public async Task Insert(TodoItem todoItem)
+        {
+            string query = @"insert into TodoItems (title, completed) output inserted.id values (@Title, @Completed);";
+            int todoItemId = await _connection.ExecuteScalarAsync<int>(query, todoItem);
+            todoItem.Id = todoItemId;
+        }
+        public async Task Update(int id, TodoItem todoItem)
+        {
+            string query = @"update TodoItems
+                            set title = @Title,
+                            completed = @Completed 
+                            where id = @Id;";
+            await _connection.ExecuteAsync(query, new
+            {
+                Id = id,
+                todoItem.Title,
+                todoItem.Completed
+            });
+        }
+        public async Task Delete(int id)
+        {
+            string query = @"delete from TodoItems where id = @Id;";
+            await _connection.ExecuteAsync(query, new
+            {
+                Id = id
+            });
+        }
+
+    }
+}
