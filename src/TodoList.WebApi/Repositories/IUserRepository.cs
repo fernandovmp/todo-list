1300b71fd410e058fbb0ccaaf267edc22bd3a9fd
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-11 17:31:26 -0300

    Add users repository

---
+++ src/TodoList.WebApi/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoList.WebApi.Models;
+
+namespace TodoList.WebApi.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByUsername(string username);
+        Task<User> GetUserWithPasswordByUsername(string username);
+        Task Insert(User user, string password);
+        Task<bool> Exists(int id);
+    }
+}
---
+++ src/TodoList.WebApi/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using TodoList.WebApi.Models;
+
+namespace TodoList.WebApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public UserRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public async Task<bool> Exists(int id)
+        {
+            string query = @"select 1 from Users where id = @Id;";
+            bool result = await _connection.ExecuteScalarAsync<bool>(query, new { id = id });
+            return result;
+        }
+
+        public async Task<User> GetUserByUsername(string username)
+        {
+            string query = @"select id, username from Users where username = @Username;";
+            User user = await _connection.QuerySingleOrDefaultAsync<User>(query, new
+            {
+                Username = username,
+            });
+            return user;
+        }
+
+        public async Task<User> GetUserWithPasswordByUsername(string username)
+        {
+            string query = @"select id, password, username from Users where username = @Username;";
+            User user = await _connection.QuerySingleOrDefaultAsync<User>(query, new
+            {
+                Username = username,
+            });
+            return user;
+        }
+
+        public async Task Insert(User user, string password)
+        {
+            string query = @"insert into Users (username, password) output inserted.id values (@Username, @Password);";
+            int userId = await _connection.ExecuteScalarAsync<int>(query, new
+            {
+                user.Username,
+                Password = password
+            });
+            user.Id = userId;
+        }
+    }
+}
