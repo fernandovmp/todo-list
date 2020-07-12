96f981e4f54c295c16ba11495b2d18425ea9b45f
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-12 16:38:07 -0300

    Add not null constraint to userId in TodoItems table

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -13,7 +13,7 @@ create table TodoItems (
     id int identity primary key,
     title varchar(100) not null,
     completed bit not null,
-    userId int foreign key references Users,
+    userId int foreign key references Users not null,
     createdOn datetime2(2) default SYSDATETIME()
 )
 go
