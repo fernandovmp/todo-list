9b93b29417944483007df417d93c86b9dbb411a1
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-18 16:20:53 -0300

    Integrate login page with backend

--- src/todo-list-spa/src/pages/Login/index.tsx
+++ src/todo-list-spa/src/pages/Login/index.tsx
@@ -2,15 +2,39 @@ import React, { useState } from 'react';
 import { FormInput } from './FormInput';
 import logo from '../../assets/done_outline-black.svg';
 import './styles.css';
+import { todoApiFactory } from '../../services/todoApi';
+import { useHistory } from 'react-router-dom';
 
 export const Login: React.FC = () => {
     const [isLogin, setIsLogin] = useState(true);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const history = useHistory();
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (isLogin) handleLogin();
+        else handleSignUp();
+    };
+
+    const handleLogin = async () => {
+        const { authenticate } = todoApiFactory();
+        const response = await authenticate(username, password);
+        if (response.status === 'success' && response.data) {
+            const { token } = response.data;
+            window.sessionStorage.setItem('token', token);
+            history.push('/');
+        }
+    };
+
+    const handleSignUp = async () => {
+        if (password !== confirmPassword) return;
+        const { signUp } = todoApiFactory();
+        const response = await signUp(username, password, confirmPassword);
+        if (response.status === 'success') {
+            await handleLogin();
+        }
     };
 
     return (
