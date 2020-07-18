759030d0c19613b83cd0594cb880c8a54e14e1b6
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-18 16:21:14 -0300

    Integrate todo list page with backend

--- src/todo-list-spa/src/pages/TodoList/index.tsx
+++ src/todo-list-spa/src/pages/TodoList/index.tsx
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/done_outline-black.svg';
 import close from '../../assets/close.svg';
 import add from '../../assets/add-black-36dp.svg';
 import './styles.css';
 import { TodoItem } from '../../models';
+import { todoApiFactory } from '../../services/todoApi';
+import { useHistory } from 'react-router-dom';
 
 interface ITodoItemComponentProps {
     todoItem: TodoItem;
     onDelete: (todoItem: TodoItem) => void;
+    onMarkAsCompleted: (todoItem: TodoItem) => void;
 }
 
 export const TodoItemComponent: React.FC<ITodoItemComponentProps> = ({
     todoItem,
     onDelete,
+    onMarkAsCompleted,
 }) => {
     const [completed, setCompleted] = useState(todoItem.completed);
 
     const handleCompleted = () => {
+        onMarkAsCompleted({ ...todoItem, completed: !completed });
         setCompleted(!completed);
     };
 
@@ -46,21 +51,72 @@ export const TodoItemComponent: React.FC<ITodoItemComponentProps> = ({
 export const TodoList: React.FC = () => {
     const [todos, setTodos] = useState<TodoItem[]>([]);
     const [todoInput, setTodoInput] = useState('');
-    const [idCount, setIdCount] = useState(0);
+    const [token] = useState(() => {
+        return window.sessionStorage.getItem('token') ?? '';
+    });
+    const history = useHistory();
 
-    const handleCreateTodo = () => {
-        const newTodo: TodoItem = {
-            id: idCount,
+    useEffect(() => {
+        const { getTodos } = todoApiFactory(token);
+        getTodos().then((result) => {
+            if (result.status !== 'success') {
+                history.push('/login');
+                return;
+            }
+            setTodos(result.data!);
+        });
+    }, [history, token]);
+
+    const handleCreateTodo = async () => {
+        const { createTodo } = todoApiFactory(token);
+        const todo: TodoItem = {
+            id: 0,
             title: todoInput,
             completed: false,
         };
-        setIdCount(idCount + 1);
         setTodoInput('');
+
+        const response = await createTodo(todo);
+
+        if (
+            response.status === 'unauthorized' ||
+            response.status === 'forbid'
+        ) {
+            history.push('/login');
+            return;
+        }
+
+        if (response.status !== 'success') return;
+
+        const newTodo = response.data!;
         setTodos([...todos, newTodo]);
     };
 
-    const handleDeleteTodo = (todoItem: TodoItem) => {
-        setTodos(todos.filter((todo) => todo.id != todoItem.id));
+    const handleDeleteTodo = async (todoItem: TodoItem) => {
+        const { deleteTodo } = todoApiFactory(token);
+        const response = await deleteTodo(todoItem.id);
+
+        if (
+            response.status === 'unauthorized' ||
+            response.status === 'forbid'
+        ) {
+            history.push('/login');
+            return;
+        }
+
+        if (response.status !== 'success') return;
+        setTodos(todos.filter((todo) => todo.id !== todoItem.id));
+    };
+
+    const handleCompleteTodo = async (todoItem: TodoItem) => {
+        const { updateTodo } = todoApiFactory(token);
+        const response = await updateTodo(todoItem);
+        if (
+            response.status === 'unauthorized' ||
+            response.status === 'forbid'
+        ) {
+            history.push('/login');
+        }
     };
 
     return (
@@ -75,6 +131,7 @@ export const TodoList: React.FC = () => {
                             <TodoItemComponent
                                 todoItem={todo}
                                 onDelete={handleDeleteTodo}
+                                onMarkAsCompleted={handleCompleteTodo}
                             />
                         </li>
                     ))}
