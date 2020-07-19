3dbaacb7fa98c0e47520a4e85f298d5b9a7c65c8
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-19 17:08:53 -0300

    Add login timeout to init database command

--- database/scripts/entrypoint.sh
+++ database/scripts/entrypoint.sh
@@ -6,6 +6,6 @@ echo "Waiting to SQL Server initialization..."
 sleep 80
 
 echo "Initializing database"
-/opt/mssql-tools/bin/sqlcmd -U sa -P  $SA_PASSWORD -i /scripts/init.sql
+/opt/mssql-tools/bin/sqlcmd -U sa -P  $SA_PASSWORD -l 30 -i /scripts/init.sql
 
 wait $pid
